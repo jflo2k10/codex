f92106bf893f83093e7f663cd6a62f77630ae618
James Floss <jbflo2k10@yahoo.com>
2016-02-07 00:11:05 -0500

    initial set up no pojs

--- scripts.js
+++ scripts.js
@@ -1,18 +1,14 @@
-/*(function($, root, undefined) {
+(function($, root, undefined) {
 	$(function() {
 		'use strict';
 				
 		
 		//jQuery
-		//$('.menu-toggle').click(function() {
-  	//	$(this).toggleClass('x');
-		//});
+		$('.menu-toggle').click(function() {
+  		$(this).toggleClass('x');
+		});
 		
 		
 	});
 
-})(jQuery, this);*/
-
-var button = document.getElementById('hamburger-menu-animation-var-1');
-
-console.log(button.length);
+})(jQuery, this);
