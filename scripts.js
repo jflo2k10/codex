b1602b5dc770cfc82d82aae315215f6353a0e16a
James Floss <jbflo2k10@yahoo.com>
2016-02-06 23:14:36 -0500

    animation 1 in pojs

--- scripts.js
+++ scripts.js
@@ -4,11 +4,24 @@
 				
 		
 		//jQuery
-		$('.menu-toggle').click(function() {
-  		$(this).toggleClass('x');
-		});
+		//$('.menu-toggle').click(function() {
+  	//	$(this).toggleClass('x');
+		//});
 		
 		
 	});
 
 })(jQuery, this);
+
+var container = document.getElementById( 'site-navigation' ),
+button = container.getElementsByClassName( 'menu-toggle' )[0];
+
+	button.onclick = function() {
+		if ( -1 !== container.className.indexOf( 'toggled' ) ) {
+			container.className = container.className.replace( ' toggled', '' );
+			button.className = button.className.replace( /(?:^|\s)x(?!\S)/g , '' );
+		} else {
+			container.className += ' toggled';
+			button.className += " x";
+		}
+  };
